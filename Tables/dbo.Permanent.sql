a02fd896631ebde222b75c068d9100c9457effc7
аа <>
2017-09-13 10:36:17 +0300

    12

---
+++ Tables/dbo.Permanent.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Permanent] (
+  [PAN] [varchar](50) NULL,
+  [number] [char](20) NULL,
+  [account] [varchar](50) NULL,
+  [permanent] [ntext] NULL,
+  [account number] [text] NULL
+)
+ON [PRIMARY]
+TEXTIMAGE_ON [PRIMARY]
+GO
---
+++ Tables/dbo.sysdiagrams.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[sysdiagrams] (
+  [name] [sysname] NOT NULL,
+  [principal_id] [int] NOT NULL,
+  [diagram_id] [int] IDENTITY,
+  [version] [int] NULL,
+  PRIMARY KEY CLUSTERED ([diagram_id]),
+  CONSTRAINT [UK_principal_name] UNIQUE ([principal_id], [name])
+)
+ON [PRIMARY]
+GO
