c887380268672066f9cb7289192d8610095fae6e
1 <1>
2019-06-18 15:25:01 +0300

    4

---
+++ Tables/dbo.crop_application3.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[crop_application3] (
+  [id] [int] IDENTITY,
+  [sequence_number] [int] NULL,
+  [record_created_date_time] [datetime] NULL,
+  [crop_season_year] [char](10) NULL,
+  CONSTRAINT [PK_crop_application3_id] PRIMARY KEY CLUSTERED ([id])
+)
+ON [PRIMARY]
+GO
