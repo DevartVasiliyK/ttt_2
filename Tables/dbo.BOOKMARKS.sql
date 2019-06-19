b6d3616e25804844343f931365a38c49de6e93e8
111 <111>
2019-06-19 07:57:14 +0300

    1234231

---
+++ Tables/dbo.BOOKMARKS.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[BOOKMARKS] (
+  [idBookmark] [numeric](10) NOT NULL,
+  [Type] [numeric](10) NOT NULL,
+  [idUser] [numeric](10) NULL,
+  [Name] [nvarchar](32) NOT NULL,
+  [Description] [nvarchar](512) NULL,
+  [BranchName] [nvarchar](255) NOT NULL,
+  [RepoPath] [nvarchar](2048) NOT NULL,
+  [DisplayOrder] [numeric](10) NULL,
+  CONSTRAINT [PK_BOOKMARKS] PRIMARY KEY CLUSTERED ([idBookmark])
+)
+ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[BOOKMARKS]
+  ADD CONSTRAINT [SCMUSERS_BOOKMARKS] FOREIGN KEY ([idUser]) REFERENCES [dbo].[SCMUSERS] ([idUser]) ON DELETE CASCADE
+GO
