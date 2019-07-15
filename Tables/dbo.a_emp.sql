487a67d99970e4672050a3edb84956b8a2f6daef
q <q>
2019-07-15 13:58:43 +0300

    c

---
+++ Tables/dbo.a_emp.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[a_emp] (
+  [id] [int] NOT NULL,
+  [name] [varchar](200) NULL,
+  [dept] [int] NULL,
+  [c1] [nchar](10) NULL,
+  CONSTRAINT [PK__a_emp__3213E83F9AC0EC69] PRIMARY KEY CLUSTERED ([id])
+)
+ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[a_emp]
+  ADD CONSTRAINT [FK_a_emp_dept] FOREIGN KEY ([dept]) REFERENCES [dbo].[b_dept] ([id])
+GO
